6c95e0d2683872b0593a963458ccb4a5d3a56872
Albina <azamatovaalbina7@gmail.com>
2022-10-15 19:22:45 +0600

    example

--- src/components/Block.js
+++ src/components/Block.js
@@ -1,7 +1,7 @@
 import "./Block.css";
 
 function Block(props) {
-  const classes = props.className + " block"; 
+  const classes = props.className + " block"; // // this is my propject eeeeevvvv deeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
   return <div className={classes}>{props.children}</div>;
 }
 export default Block;
