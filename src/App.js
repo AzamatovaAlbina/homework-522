9da02ecfb07cc2ac651c2dfe08084e020ee7927a
Albina <azamatovaalbina7@gmail.com>
2022-10-15 18:14:48 +0600

    render color

--- src/App.js
+++ src/App.js
@@ -1,47 +1,48 @@
 import "./App.css";
-import BottomPart from "./components/BottomPart";
-import TopPart from "./components/TopPart";
-const comment = [
+import Block from "./components/Block";
+
+const blocks = [
   {
-    date: new Date(),
-    text: "I hope you enjoy learning React!",
-    author: {
-      name: "Hello Kitty",
-      avatarUrl:
-        "https://i.pinimg.com/736x/1e/58/76/1e5876f5d9e9ae637f54565bf2051e5e.jpg",
-    },
+    id: "b2",
+    block: "green",
   },
   {
-    date: new Date(),
-    text: "I hope you enjoy learning React!",
-    author: {
-      name: "Hello Kitty",
-      avatarUrl:
-        "https://imageio.forbes.com/specials-images/imageserve/09zFfq433L08b/960x960.jpg?format=jpg&width=960",
-    },
+    id: "b3",
+    block: "color",
   },
   {
-    date: new Date(),
-    text: "I hope you enjoy learning React!",
-    author: {
-      name: "Hello Kitty",
-      avatarUrl:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTq2CIDapM-a9OkoYGdndeyc6-zpA6z5rMsD5mZmhCuU8F4XfWNnq8xOU2QRJvT2BWvCV4&usqp=CAU",
-    },
+    id: "b4",
+    block: `${Math.random()}`,
+  },
+  {
+    id: "b5",
+    block: "green",
+  },
+  {
+    id: "b6",
+    block: `${Math.random()}`,
+  },
+  {
+    id: "b1",
+    block: "yellow",
   },
 ];
+
 
 function App() {
   return (
     <div className="App">
-      {comment.map((item) => (
-        <div className="container">
-          <TopPart avatarUrl={item.author.avatarUrl}
-          name={item.author.name}/>
-          <BottomPart text={item.text}
-          date={item.date}/>
-        </div>
-      ))}
+      {blocks.map((item) =>
+        item.block === "yellow" ? (
+          <Block className="yellow" key={item.id} />
+        ) : item.block === "green" ? (
+          <Block className="green" key={item.id} />
+        ) : item.block === "color" ? (
+          <Block className="lime" key={item.id} />
+        ) : (
+          <Block className="red" key={item.id} />
+        )
+      )}
     </div>
   );
 }
---
+++ src/components/Block.js
@@ -0,0 +1,7 @@
+import "./Block.css";
+
+function Block(props) {
+  const classes = props.className + " block"; 
+  return <div className={classes}>{props.children}</div>;
+}
+export default Block;
