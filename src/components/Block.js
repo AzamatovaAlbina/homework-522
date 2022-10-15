7636b7a6bc03c6b45e048aac8ca18261a1c9b417
Albina <azamatovaalbina7@gmail.com>
2022-10-15 23:59:20 +0600

    render color

--- src/components/Block.js
+++ src/components/Block.js
@@ -1,7 +1,7 @@
 import "./Block.css";
 
 function Block(props) {
-  const classes = props.className + " block"; // // this is my propject eeeeevvvv deeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+  const classes = props.className + " block"; 
   return <div className={classes}>{props.children}</div>;
 }
 export default Block;
